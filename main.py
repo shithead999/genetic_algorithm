54133c9b42887f48f6cbdac2875b612e1be60995
nanorex07 <jainnamah@gmail.com>
2022-08-17 23:51:06 +0530

    done with project

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from DNA import DNA
 from population import Population
+import time
+from tkinter import Tk
+from ui import App
+import math
 
 # variables
-phrase = "Out of the box."
-pop_size = 10
+phrase = "Switch to God Mode."
+pop_size = 1000
+mutation_rate = 0.01
 
 def main():
-	p = Population(phrase, pop_size)
-	p.calc_fitness()
-	print("Best Fitness:", str(p.best_fitness))
-	print("Best Fit DNA:", p.best_fit_dna.get_str())
-	p.show_pop()
+	win = Tk()
+	app = App(win)
+	win.title("Genetic Algorithm")
+	p = Population(mutation_rate, phrase, pop_size, app)
+	while p.running:
+		win.update()
+		p.calc_fitness()
+		p.natural_selection()
+		p.generate()
+		app.best_fit.set("Best Fitness: " + str(math.floor(p.best_fitness*100)) + "%" )
+		app.generation.set("Generation: " + str(p.generation))
+				
+		p.show_pop()
+		time.sleep(0.2)
+	win.mainloop()
 
 
 if __name__ == "__main__":
