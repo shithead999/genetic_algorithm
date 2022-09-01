cd09e5fe67d7bcbbaad8b7cc867b3d3b2bfc35cf
Namah Jain <33959345+nanorex07@users.noreply.github.com>
2022-09-01 21:01:36 +0530

    Update main.py

--- genetic_algorithm/smart_rockets_eg/main.py
+++ genetic_algorithm/smart_rockets_eg/main.py
@@ -1,30 +1,40 @@
 from tkinter import Canvas, Tk
 from settings import *
-from rocket import Rocket
 import math
-
+from population import Population
+from pillars import Pillar
 
 class Main:
 	def __init__(self, win):
 		self.win  = win
 		self.win.resizable(0,0)
-		self.c = Canvas(self.win, bg="#F9F6EE", bd=0, highlightthickness=0, width=WIDTH, height=HEIGHT)
+		self.c = Canvas(self.win, bg=COLORS[3], bd=0, highlightthickness=0, width=WIDTH, height=HEIGHT)
 		self.c.pack(fill="both", expand=True)
-		self.g_index = 0
-		self.a = [Rocket(self.c) for i in range(100)]
 
-	# this is the loop of the program
+		self.target = self.c.create_rectangle(
+			[TARGET_COOR[0]-TARGET_SIZE, TARGET_COOR[1]-TARGET_SIZE, TARGET_COOR[0]+TARGET_SIZE, TARGET_COOR[1]+TARGET_SIZE],
+			fill=COLORS[0],
+			outline=None,
+			width=0,
+		)
+
+		self.pillars = [
+			Pillar(self.c, WIDTH/2 - 200, HEIGHT*0.40, 400, 20),
+			# Pillar(self.c, WIDTH/2 - 220, HEIGHT*0.30, 20, 200),
+			Pillar(self.c, WIDTH/2 - 100, HEIGHT*0.50, 20, 200),
+		]
+		self.population = Population(self.c, self.pillars)
+
 	def start(self):
-		# update canvas for animation
 		self.c.update()
+		if self.population.g_index < FORCE_COUNT:
+			self.population.update()
+		else:
+			# perform genetic algorithm to get new generation
+			self.population.calc_fitness()
+			self.population.natural_selection()
+			self.population.generate()
 
-		# apply forces (genes) to agents while all are not applied
-		if self.g_index < FORCE_COUNT:
-			for i in self.a:
-				i.update(self.g_index)
-			self.g_index += 1
-
-		# sleep time to control FPS
 		self.win.after(TIME_DELAY, self.start)
 
 
